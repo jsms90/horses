14a1675e9489ee6137a804932a4e71e4434198c9
ameliejyc <amelie.chan@gmail.com>
2017-10-19 11:58:45 +0100

    #4 Create model :green_apple:

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Http exposing (..)
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    text "hey sexy horses"
+    Html.program
+        { init = init
+        , view = view
+        , subscriptions = always Sub.none
+        , update = update
+        }
+
+
+
+-- model
+
+
+type alias Model =
+    { character : Maybe String
+    , gifUrls : List String
+    }
+
+
+model : Model
+model =
+    { character = Nothing
+    , gifUrls = []
+    }
+
+
+
+-- update
+-- view
